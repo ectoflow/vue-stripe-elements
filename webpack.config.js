36232eae2e8d44877df63c367227a7ebc45fbed9
Oleksandr Bystrikov <coder@yakviter.com.ua>
2021-03-25 13:01:04 +0100

    refactor(dependencies): upgrade webpack, remove unused lodash

--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,35 @@
-const webpack = require('webpack');
-const path = require('path');
+const path = require('path')
+const TerserPlugin = require('terser-webpack-plugin')
+const { VueLoaderPlugin } = require('vue-loader')
 
 module.exports = {
   entry: './src/index.js',
+  mode: 'production',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'index.js',
     library: 'vue-stripe-elements',
     libraryTarget: 'umd',
-    umdNamedDefine: true
+    umdNamedDefine: true,
   },
   resolve: {
-    extensions: ['.js', '.vue']
+    extensions: ['.js', '.vue'],
   },
   module: {
-    loaders: [{
-      test: /\.js$/,
-      loader: 'babel-loader',
-      include: __dirname,
-      exclude: /node_modules/
-    }, {
-      test: /\.vue$/,
-      loader: 'vue-loader'
-    }]
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader',
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+      },
+    ],
   },
-  plugins: [
-    new webpack.optimize.UglifyJsPlugin({
-      minimize: true,
-      sourceMap: false,
-      mangle: true,
-      compress: {
-          warnings: false
-      }
-    })
-  ]
-};
+  optimization: {
+    minimize: true,
+    minimizer: [new TerserPlugin()],
+  },
+  plugins: [new VueLoaderPlugin()],
+}
