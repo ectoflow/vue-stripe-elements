e81c04d29a883923c3bbacfd4348a457252bf9f4
Oleksandr Bystrikov <coder@yakviter.com.ua>
2021-03-25 12:55:23 +0100

    refactor(dev): use prettier and eslint

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+  },
+  parserOptions: {
+    parser: 'babel-eslint',
+  },
+  extends: [
+    'plugin:vue/recommended',
+    'plugin:prettier/recommended',
+    'prettier',
+  ],
+  // required to lint *.vue files
+  plugins: ['vue'],
+  rules: {
+    semi: [2, 'never'],
+  },
+}
