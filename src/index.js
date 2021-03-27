c9a5836124d2fc3235f1d88098fd36648086e698
Oleksandr Bystrikov <coder@yakviter.com.ua>
2021-03-27 18:01:01 +0100

    refactor: remove helper components, too much hassle, use StripeElement

--- src/index.js
+++ src/index.js
@@ -1,18 +1,10 @@
 import StripeElement from './components/StripeElement'
 import StripeElements from './components/StripeElements'
-import Card from './components/Card'
-import CardExpiry from './components/CardExpiry'
-import CardCvc from './components/CardCvc'
-import CardNumber from './components/CardNumber'
 import { initStripe, createElements, createElement } from './stripeElements'
 
 export {
   StripeElements,
   StripeElement,
-  Card,
-  CardNumber,
-  CardExpiry,
-  CardCvc,
   initStripe,
   createElements,
   createElement,
